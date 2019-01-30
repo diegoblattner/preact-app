71db6c6596379128bf61d9433912c935fd7476ac
Diego Blattner <diegoblattner@hotmail.com>
2019-01-30 01:59:21 +0000

    Adding an express server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+// 'build' was created during the build phase
+app.use(express.static('build'));
+
+// error handling
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app;
