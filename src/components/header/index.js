7838309deb215ae6fdecdb81054e87b2177db9fb
Diego Blattner <diegoblattner@hotmail.com>
2019-01-31 01:15:15 +0000

    feat(nav): removing links

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,18 +4,7 @@ import style from './style.css';
 
 const Header = () => (
   <header className={style.header}>
-    <h1>Preact App</h1>
-    <nav>
-      <Link activeClassName={style.active} href="/">
-        Home
-      </Link>
-      <Link activeClassName={style.active} href="/profile">
-        Me
-      </Link>
-      <Link activeClassName={style.active} href="/profile/john">
-        John
-      </Link>
-    </nav>
+    <h1 className={style.header__title}>Preact App</h1>
   </header>
 );
 
